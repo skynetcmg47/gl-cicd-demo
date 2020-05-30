16f13beecc02895f3badf714e2b077f83644fac4
Nam Phuong <skynetcmg47@gmail.com>
2020-05-30 08:55:30 +0700

    First blood

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const response = {
+  message: 'Hello World!'
+}
+app.get('/', (req, res) => res.send(response))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
